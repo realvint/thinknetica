0fda17eefff2f9530bae89ba58ddc4e4fd874e5b
realvint <victor.malukhin@gmail.com>
2021-06-16 23:48:30 +0300

    add main menu interface

---
+++ Lesson_4/cargo_train.rb
@@ -0,0 +1,9 @@
+require_relative 'train'
+
+class CargoTrain < Train
+
+  def initialize(number)
+    super(number)
+    @type = "cargo"
+  end
+end
---
+++ Lesson_4/passenger_train.rb
@@ -0,0 +1,9 @@
+require_relative 'train'
+
+class PassengerTrain < Train
+
+  def initialize(number)
+    super(number)
+    @type = "passenger"
+  end
+end
